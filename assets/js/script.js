3c8f1ca84a8cc9b240cd8d413e9e74e12f32e119
Majid Kargar <52089654+fulcain@users.noreply.github.com>
2022-11-27 16:52:58 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// grabing a hold of elements
+const addNewNoteEl = document.querySelector("#add-new-note-input")
+const addBtnEl = document.querySelector("#add-note-btn")
+const deleteBtnEl = document.querySelector("#delete-note-btn")
+const editBtnEL = document.querySelector("#edit-note-btn")
